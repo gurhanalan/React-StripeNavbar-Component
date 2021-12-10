935c0a1fbdc53df81d3fecb34ca0bb53ed8dd24a
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-12-10 18:11:22 +0300

    Updates

---
+++ precache-manifest.9f3a61c85134112324590c84ba8641b4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54e14a847b18d30478fa8d9c3d99852",
+    "url": "/React-StripeNavbar-Component/index.html"
+  },
+  {
+    "revision": "2501a5a7a3e0ed0c7464",
+    "url": "/React-StripeNavbar-Component/static/css/main.aa602e33.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/React-StripeNavbar-Component/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-StripeNavbar-Component/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2501a5a7a3e0ed0c7464",
+    "url": "/React-StripeNavbar-Component/static/js/main.313a5bcf.chunk.js"
+  },
+  {
+    "revision": "66b357f5728cee9a7a72",
+    "url": "/React-StripeNavbar-Component/static/js/runtime-main.7a6c69ff.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "/React-StripeNavbar-Component/static/media/hero.8596ab4a.svg"
+  },
+  {
+    "revision": "1090473dd728fcc17033df70118fa668",
+    "url": "/React-StripeNavbar-Component/static/media/logo.1090473d.svg"
+  },
+  {
+    "revision": "58d7e3d689c0cb8eb365b86046ecdce8",
+    "url": "/React-StripeNavbar-Component/static/media/phone.58d7e3d6.svg"
+  }
+]);
